2c8db05b9e81c04d060da05eb978d94c27997b26
Fyzxs <Fyzxs@users.noreply.github.com>
2017-03-29 17:57:36 -0700

    PopulcationControl#Spawn*

--- GameOfLife/CellTests.cs
+++ GameOfLife/CellTests.cs
@@ -7,9 +7,63 @@ namespace GameOfLife
     public class CellTests
     {
         [Test]
-        public void Test1()
+        public void CellShouldBeAliveGivenAlive()
         {
-            Assert.True(true);
+            Cell cell = new Cell(true);
+            bool isAlive = cell.IsAlive();
+            Assert.True(isAlive);
+        }
+
+        [Test]
+        public void CellShouldBeDeadGivenDead()
+        {
+            Cell cell = new Cell(false);
+            bool isAlive = cell.IsAlive();
+            Assert.False(isAlive);
+        }
+
+        [Test]
+        public void ShouldSpawnLivingCell()
+        {
+            Cell cell = new PopulationControl().SpawnLivingCell();
+            bool isAlive = cell.IsAlive();
+            Assert.True(isAlive);
+        }
+
+        [Test]
+        public void ShouldSpawnDeadCell()
+        {
+            Cell cell = new PopulationControl().SpawnDeadCell();
+            bool isAlive = cell.IsAlive();
+            Assert.False(isAlive);
+        }
+    }
+
+    public class PopulationControl
+    {
+        public Cell SpawnLivingCell()
+        {
+            return new Cell(true);
+        }
+
+        public Cell SpawnDeadCell()
+        {
+            return new Cell(false);
+        }
+    }
+
+    public class Cell
+    {
+        private readonly bool _isAlive;
+
+        public Cell(bool isAlive)
+        {
+            _isAlive = isAlive;
+        }
+
+        public bool IsAlive()
+        {
+            return _isAlive;
         }
     }
 }
