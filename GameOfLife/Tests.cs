bd3b459ee90722213d142257dadb3c28abaec283
Fyzxs <Fyzxs@users.noreply.github.com>
2017-03-29 17:44:40 -0700

    initial commit

---
+++ GameOfLife/Tests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace GameOfLife
+{
+    [TestFixture]
+    public class CellTests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
